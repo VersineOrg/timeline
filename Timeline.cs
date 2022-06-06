61b7ed4b0116b1540cb3e704b33c025078ee7a8f
Maxime Trimboli <maximetrimboli@gmail.com>
2022-06-06 12:07:37 +0200

    changemen

--- Timeline.cs
+++ Timeline.cs
@@ -50,7 +50,7 @@ public class Timeline
     }
     public static string TimelineToJson(List<BsonDocument> postsBson)
     {
-        string jsonString = "";
+        string jsonString = "[";
         int l = postsBson.Count;
         for (int i = 0; i < l; i++)
         {
@@ -60,6 +60,8 @@ public class Timeline
                 jsonString += ", ";
             }
         }
+
+        jsonString += "]";
         return jsonString;
     }
     
